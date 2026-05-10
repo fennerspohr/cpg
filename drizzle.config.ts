ef13a9d093cd338d5849ec915e1a0dc54b76a26d
fennerspohr <isadorafennerspohr@gmail.com>
2026-05-10 18:26:16 -0300

    corrige drizzle.config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,21 @@
 import pg from 'pg';
+import { defineConfig } from 'drizzle-kit';
 import { drizzle } from "drizzle-orm/node-postgres";
-import * as schema from "../db/schema";
 
+export default defineConfig({
+  dialect: 'postgresql', // or 'mysql' | 'sqlite'
+  schema: './server/db/schema.ts',
+  out: './server/db/migrations',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
 // Tentamos ler do .env, se não existir, usamos a string baseada na sua imagem
 const connectionString = process.env.DATABASE_URL || "postgres://postgres:1457@localhost:5432/cpg";
 
-const pool = new pg.Pool({
-  connectionString: connectionString,
-});
+// const pool = new pg.Pool({
+//   connectionString: connectionString,
+// });
 
-export const db = drizzle(pool, { schema });
-export const tables = schema;
+// export const db = drizzle(pool, { schema });
+// export const tables = schema;
